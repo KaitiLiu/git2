79a3ab724c718d9cfff6d343f43109df08282a84
kaitiLiu <kaitiliu.au@gmail.com>
2023-02-14 07:41:11 +0000

    question9

--- question09.sh
+++ question09.sh
@@ -1,5 +1,5 @@
-git branch branch01
-git checkout branch01
-touch file03
-git add file03
-git commit -m "file03"
+git branch branch1
+git checkout branch1
+touch file3
+git add file3
+git commit -m "file3"
